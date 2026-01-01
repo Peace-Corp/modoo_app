67b40282da174a7b30371a7802b84c3386142252
dldnlee <sungeun8877@gmail.com>
2026-01-02 02:16:29 +0900

    feat: overall flow fix - changing routes resets the global canvas state. - added cloud convert api for ai files - added loading modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import NavigationListener from "./components/NavigationListener";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavigationListener />
         <div className="w-full lg:max-w-7xl lg:mx-auto">
           <main>{children}</main>
           <Footer />
