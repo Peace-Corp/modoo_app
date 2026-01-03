98eb99257d795f492bae8490ecd8e12d9933a7da
dldnlee <sungeun8877@gmail.com>
2026-01-04 00:17:21 +0900

    design: remove black bg in main

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     }, 1000)
   }, [])
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         
       </main>
